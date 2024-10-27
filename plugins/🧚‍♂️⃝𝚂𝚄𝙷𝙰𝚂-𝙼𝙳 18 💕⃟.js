260c0c8f7971019903d771cb36fe0ce2e40d0f46
Suhas Pathsindu <pradeepakumari356@gmail.com>
2024-10-27 20:00:45 +0530

    💕𝚂 𝚄 𝙷 𝙰 𝚂 - 𝙼 𝙳🧚‍♂️

---
+++ plugins/🧚‍♂️⃝𝚂𝚄𝙷𝙰𝚂-𝙼𝙳 18 💕⃟.js
@@ -0,0 +1,35 @@
+//  ░██████╗██╗░░░██╗██╗░░██╗░█████╗░░██████╗    ███╗░░░███╗██████╗░    ██╗░░░██╗░█████╗░
+//  ██╔════╝██║░░░██║██║░░██║██╔══██╗██╔════╝    ████╗░████║██╔══██╗    ██║░░░██║██╔══██╗
+//  ╚█████╗░██║░░░██║███████║███████║╚█████╗░    ██╔████╔██║██║░░██║    ╚██╗░██╔╝╚█████╔╝
+//  ░╚═══██╗██║░░░██║██╔══██║██╔══██║░╚═══██╗    ██║╚██╔╝██║██║░░██║    ░╚████╔╝░██╔══██╗
+//  ██████╔╝╚██████╔╝██║░░██║██║░░██║██████╔╝    ██║░╚═╝░██║██████╔╝    ░░╚██╔╝░░╚█████╔╝
+//  ╚═════╝░░╚═════╝░╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░    ╚═╝░░░░░╚═╝╚═════╝░    ░░░╚═╝░░░░╚════╝░
+
+//  ░██╗░░░░░░░██╗██╗░░██╗░█████╗░████████╗░██████╗░█████╗░██████╗░██████╗░    ██████╗░░█████╗░████████╗
+//  ░██║░░██╗░░██║██║░░██║██╔══██╗╚══██╔══╝██╔════╝██╔══██╗██╔══██╗██╔══██╗    ██╔══██╗██╔══██╗╚══██╔══╝
+//  ░╚██╗████╗██╔╝███████║███████║░░░██║░░░╚█████╗░███████║██████╔╝██████╔╝    ██████╦╝██║░░██║░░░██║░░░
+//  ░░████╔═████║░██╔══██║██╔══██║░░░██║░░░░╚═══██╗██╔══██║██╔═══╝░██╔═══╝░    ██╔══██╗██║░░██║░░░██║░░░
+//  ░░╚██╔╝░╚██╔╝░██║░░██║██║░░██║░░░██║░░░██████╔╝██║░░██║██║░░░░░██║░░░░░    ██████╦╝╚█████╔╝░░░██║░░░
+//  ░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝░░░╚═╝░░░╚═════╝░╚═╝░░╚═╝╚═╝░░░░░╚═╝░░░░░    ╚═════╝░░╚════╝░░░░╚═╝░░░
+
+
+
+// * Project name - SUHAS- MD 
+// * Author - Suhas Pathsindu
+// * Team - Suhas Bro 
+// * Version - V2
+
+// World best and powerfull whatsapp user bot in Sri lanka
+// 🧬©ꜱᴜʜᴀꜱ-ᴍᴅ ʙʏ ꜱᴜᴀʜꜱ ᴘᴀᴛʜꜱɪɴᴅᴜッ
+
+
+
+
+
+
+
+
+
+
+
+const _0x10cd01=_0x2dc3;function _0x50e9(){const _0x5ca44f=['googlepw','Generate\x20a\x20strong\x20password.','2499695mnmaxx','339LLLxIf','../command','message','sendMessage','364962VmmLFX','crypto','abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+[]{}|;:,.<>?','🔐\x20*Your\x20Strong\x20Password*\x20🔐\x0a\x0aPlease\x20find\x20your\x20generated\x20password\x20below:\x0a\x0a\x0a>\x20*©\x20𝙿𝚘𝚠𝚎𝚛𝚍\x20𝙱𝚢\x20🧚‍♂️⃝𝚂𝚄𝙷𝙰𝚂-𝙼𝙳\x20𝚅8\x20💕⃟*','830384XUqfro','499622UirHHo','636699WLnIjH','12968RJytTW','gpass','12pmdleA','7573914AngeZf','log','Please\x20provide\x20a\x20valid\x20length\x20for\x20the\x20password\x20(Minimum\x2008\x20Characters💦).'];_0x50e9=function(){return _0x5ca44f;};return _0x50e9();}function _0x2dc3(_0x2761aa,_0x1bfa31){const _0x50e998=_0x50e9();return _0x2dc3=function(_0x2dc3fd,_0xebf14e){_0x2dc3fd=_0x2dc3fd-0x139;let _0x2b77ae=_0x50e998[_0x2dc3fd];return _0x2b77ae;},_0x2dc3(_0x2761aa,_0x1bfa31);}(function(_0x3a2dcc,_0xaec418){const _0xca155c=_0x2dc3,_0x36082d=_0x3a2dcc();while(!![]){try{const _0x818c13=parseInt(_0xca155c(0x13c))/0x1+parseInt(_0xca155c(0x14b))/0x2+parseInt(_0xca155c(0x147))/0x3*(-parseInt(_0xca155c(0x13e))/0x4)+parseInt(_0xca155c(0x146))/0x5+-parseInt(_0xca155c(0x140))/0x6*(-parseInt(_0xca155c(0x13d))/0x7)+parseInt(_0xca155c(0x13b))/0x8+-parseInt(_0xca155c(0x141))/0x9;if(_0x818c13===_0xaec418)break;else _0x36082d['push'](_0x36082d['shift']());}catch(_0x2d41eb){_0x36082d['push'](_0x36082d['shift']());}}}(_0x50e9,0x3f716));const crypto=require(_0x10cd01(0x14c)),{cmd}=require(_0x10cd01(0x148));cmd({'pattern':_0x10cd01(0x13f),'alias':[_0x10cd01(0x144)],'desc':_0x10cd01(0x145),'category':'other','react':'🔐','filename':__filename},async(_0x377e0a,_0x10fe57,_0x399c41,{from:_0x3e831d,quoted:_0x1fa007,body:_0x305e0f,isCmd:_0x8f3614,command:_0x120513,args:_0x5e97f8,q:_0x56b96e,isGroup:_0x4ac21e,sender:_0x1216ab,senderNumber:_0x41c129,botNumber2:_0x1e44fa,botNumber:_0x3afc31,pushname:_0x15bb88,isMe:_0xfa7cc,isOwner:_0x722814,groupMetadata:_0x3b1a29,groupName:_0x25676f,participants:_0x1e0e9e,groupAdmins:_0x145951,isBotAdmins:_0x2520ec,isAdmins:_0x3054ab,reply:_0x585dc6})=>{const _0x1f62cb=_0x10cd01;try{const _0x49fbfb=_0x5e97f8[0x0]?parseInt(_0x5e97f8[0x0]):0xc;if(isNaN(_0x49fbfb)||_0x49fbfb<0x8)return _0x585dc6(_0x1f62cb(0x143));const _0x44cdf9=_0x5cfc38=>{const _0x52b0e7=_0x1f62cb,_0x4709cb=_0x52b0e7(0x139);let _0x1ecda4='';for(let _0x12c1dc=0x0;_0x12c1dc<_0x5cfc38;_0x12c1dc++){const _0x4d5761=crypto['randomInt'](0x0,_0x4709cb['length']);_0x1ecda4+=_0x4709cb[_0x4d5761];}return _0x1ecda4;},_0x386ab0=_0x44cdf9(_0x49fbfb),_0x16c4f4=_0x1f62cb(0x13a);await _0x377e0a[_0x1f62cb(0x14a)](_0x3e831d,{'text':_0x16c4f4},{'quoted':_0x10fe57}),await _0x377e0a[_0x1f62cb(0x14a)](_0x3e831d,{'text':_0x386ab0},{'quoted':_0x10fe57}),await _0x399c41['react']('✅');}catch(_0x43d270){console[_0x1f62cb(0x142)](_0x43d270),_0x585dc6('❌\x20Error\x20generating\x20password🤕:\x20'+_0x43d270[_0x1f62cb(0x149)]);}});
